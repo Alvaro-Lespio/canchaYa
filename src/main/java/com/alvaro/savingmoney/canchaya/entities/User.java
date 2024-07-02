7a91e219effcadc96ddf3ac52455fbc9ba0c46d8
Alvaro Lespio Laffont <alvarolespio@estudiante.mdp.utn.edu.ar>
2024-07-01 23:20:06 -0300

    Update User

--- src/main/java/com/alvaro/savingmoney/canchaya/entities/User.java
+++ src/main/java/com/alvaro/savingmoney/canchaya/entities/User.java
@@ -3,10 +3,73 @@ package com.alvaro.savingmoney.canchaya.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+import java.util.Objects;
+
 @Entity
 public class User {
+    //Attribute
     @Id
     private Long id;
     private String username;
+    private String password;
+    private String email;
 
+    //Constructor
+    public User(){
+
+    }
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    //Getter and setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //Equals and Hashcode
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        User user = (User) object;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, email);
+    }
 }
